9d37a65b3d03431bfa2013f5bebaed2a35f6255b
commit 測試 <mincerafdt2730@gmail.com>
2023-03-22 11:58:20 +0800

    加入樂透功能

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var txv: TextView = findViewById(R.id.txv)
-
         txv.text = "0"
     }
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = (1..100).random().toString()
+    }
+
 }
